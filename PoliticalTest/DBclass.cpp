75b519c8c1b8ca4c9dbae67ebea8fcdf2c20b81a
kertys35 <kertys50@gmail.com>
2024-08-06 12:54:23 +0700

    Начало теста политической идеалогии

---
+++ PoliticalTest/DBclass.cpp
@@ -0,0 +1,229 @@
+#include "DBclass.h"
+
+DBclass::DBclass(QObject *parent): QObject{parent}
+, networkManager(new NetworkManager(this))
+{
+    connect(networkManager, &QNetworkAccessManager::finished, this, &DBclass::onHttpFinished); //ВАЖНО!
+}
+//
+void DBclass::connectToDatabase()
+{
+    db=QSqlDatabase::addDatabase("QPSQL");           //тип базы данных
+    db.setHostName(DATABASE_HOSTNAME);               //адрес сервера
+    db.setDatabaseName(DATABASE_NAME);               //название таблицы
+    db.setUserName(DATABASE_USERNAME);               //имя пользователя
+    db.setPassword(DATABASE_PASSWORD);               //пароль пользователя
+    db.setPort(DATABASE_PORT);
+    this->openDataBase();
+}
+//Открытие БД
+bool DBclass::openDataBase()
+{
+    if(db.open())
+    {
+        qDebug() << "Есть подключение";
+        return true;
+    }
+    else
+    {
+         qDebug() << "Нет подключения";
+        return false;
+    }
+}
+
+
+//Закрытие БД
+void DBclass::closeDataBase()
+{
+    qDebug()<<"Соединение закрыто";
+    db.close();
+}
+//Вставление данных в БД
+void DBclass::insertIntoTable(const QVariantList &data, int id)
+{
+    QUrl url("http://localhost:3000/tests/add");
+    QNetworkRequest request(url);
+
+    // Устанавливаем заголовок Content-Type
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    // Создаем JSON объект с командой и именем пользователя
+    QJsonObject json;
+    json["user_id"] = id;
+    json["firstresult"] = data[0].toInt();
+    json["secondresult"] = data[1].toInt();
+    json["thirdresult"] = data[2].toInt();
+    json["fourthresult"] = data[3].toInt();
+    json["fifthresult"] = data[4].toInt();
+
+    // Отправляем POST запрос
+    NetworkManager *networkManager = new NetworkManager(this);
+    networkManager->post(request, QJsonDocument(json).toJson());
+    qDebug()<<json;
+    qDebug()<<url;
+    flagUserID=1;
+}
+//Подготовка данных для вставки в БД
+void DBclass::insertIntoTable(const QString &FirstResult, const QString &SecondResult, const QString &ThirdResult,
+                              const QString &FourthResult, const QString &FifthResult, int id)
+{
+    QVariantList data;
+    data.append(FirstResult);
+    data.append(SecondResult);
+    data.append(ThirdResult);
+    data.append(FourthResult);
+    data.append(FifthResult);
+    insertIntoTable(data,id);
+}
+//Обновление данных в БД
+void DBclass::updateTable(const QVariantList &data, const int id)
+{
+    QString urlStr = "http://localhost:3000/tests/update/"+QString::number(id);
+    QUrl url(urlStr);
+    QNetworkRequest request(url);
+
+    // Устанавливаем заголовок Content-Type
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    // Создаем JSON объект с командой и данными теста
+    QJsonObject json;
+    json["user_id"] = id;
+    json["firstresult"] = data[0].toInt();
+    json["secondresult"] = data[1].toInt();
+    json["thirdresult"] = data[2].toInt();
+    json["fourthresult"] = data[3].toInt();
+    json["fifthresult"] = data[4].toInt();
+
+    // Отправляем PUT запрос
+    NetworkManager *networkManager = new NetworkManager(this);
+    networkManager->put(request, QJsonDocument(json).toJson());
+
+    qDebug()<<json;
+    qDebug()<<url;
+}
+//Подготовка данных для обновления БД
+void DBclass::updateTable(const QString &FirstResult, const QString &SecondResult, const QString &ThirdResult,
+                          const QString &FourthResult, const QString &FifthResult, int id)
+{
+    QVariantList data;
+    data.append(FirstResult);
+    data.append(SecondResult);
+    data.append(ThirdResult);
+    data.append(FourthResult);
+    data.append(FifthResult);
+    updateTable(data,id);
+}
+//Проверить, есть ли ID в базе данных
+bool DBclass::checkID()
+{
+
+    if(flagUserID)
+    {
+        qDebug() << "Существует ID";
+        qDebug() << flagUserID;
+
+        return true;
+    }
+    else
+    {
+        qDebug() << "Не существует ID";
+        qDebug() << flagUserID;
+        return false;
+    }
+}
+
+void DBclass::get_results(int id)
+{
+    QString userID = QString::number(id);
+    QString urlStr = "http://localhost:3000/tests/user/" + userID;
+
+    if(tempScore[0]>0)
+    {
+        flagUserID=1;
+    }
+
+    qDebug() << urlStr;
+    QUrl url(urlStr); // Укажите правильный URL вашего сервера
+    QNetworkRequest request(url);
+
+    // Устанавливаем заголовок Content-Type
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    // Создаем JSON объект с командой и именем пользователя
+    QJsonObject json;
+
+    // Отправляем GET запрос
+    networkManager->get(request, QJsonDocument(json).toJson());
+}
+
+void DBclass::onHttpFinished(QNetworkReply *reply)
+{
+    if (reply->error()) {
+        qDebug() << "Error:" << reply->errorString();
+        return;
+    }
+
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(reply->readAll());
+
+    qDebug() << "Received JSON:" << jsonDoc.toJson(QJsonDocument::Indented);
+
+    QJsonObject jsonObj;
+    if (jsonDoc.isObject())
+    {
+        jsonObj = jsonDoc.object();
+    }
+
+
+    if(jsonObj["tag"] == "tests")
+    {
+        QJsonArray jsonArray = jsonObj["data"].toArray();
+        if (jsonArray.size() > 0){
+            on_newDataReceived(jsonArray);
+        }
+    }
+}
+void DBclass::on_newDataReceived(const QJsonArray &jsonArray)
+{
+    qDebug() <<"Получены новые данные";
+    if (jsonArray.isEmpty()){
+        qDebug() << "No data available for reading!";
+        return;
+    }
+    for (int j = 0 ; j< jsonArray.size(); j++)
+    {
+        QJsonObject test = jsonArray.at(j).toObject();
+        tempScore[0] = test["firstresult"].toInt();
+        tempScore[1] = test["secondresult"].toInt();
+        tempScore[2] = test["thirdresult"].toInt();
+        tempScore[3] = test["fourthresult"].toInt();
+        tempScore[4] = test["fifthresult"].toInt();
+    }
+}
+
+//Извлечь первый результат из БД
+int DBclass::get_results1()
+{
+    return tempScore[0];
+}
+//Извлечь второй результат из БД
+int DBclass::get_results2()
+{
+    return tempScore[1];
+}
+//Извлечь третий результат из БД
+int DBclass::get_results3()
+{
+
+    return tempScore[2];
+}
+//Извлечь четвёртый результат из БД
+int DBclass::get_results4()
+{
+
+    return tempScore[3];
+}
+//Извлечь пятый результат из БД
+int DBclass::get_results5()
+{
+    return tempScore[4];
+}
---
+++ PoliticalTest/DBclass.h
@@ -0,0 +1,80 @@
+#ifndef DBCLASS_H
+#define DBCLASS_H
+
+#include <QObject>
+#include <QSql>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlDatabase>
+#include <QFile>
+#include <QDate>
+#include <QDebug>
+
+#include <QByteArray>
+#include <QString>
+#include <QScrollArea>
+#include <QStringList>
+#include <QNetworkAccessManager>
+#include <QNetworkProxy>
+#include <QNetworkReply>
+#include <QJsonValueRef>
+#include <QJsonArray>
+#include <QTimer>
+#include <QMap>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <networkmanager.h>
+#include <QMultiMap>
+#include <QListWidget>
+#include <qqml.h>
+
+#define DATABASE_HOSTNAME               "localhost"         //Название БД
+#define DATABASE_NAME                   "Test"      //Называние БД
+#define DATABASE_USERNAME               "postgres"       //Имя пользователя
+#define DATABASE_PASSWORD               "123"           //Пароль от БД
+#define DATABASE_PORT                   5433            //Порт подключения
+
+#define TABLE                           "tests"         //Название таблицы БД
+#define TABLE_NAME                      "user_id"          //Колонка с id пользователя
+#define TABLE_FIRST_SCORE               "firstresult"       //Колонка с результатами по Эксраверсии – Интроверсии
+#define TABLE_SECOND_SCORE              "secondresult"      //Колонка с результатами по Привязанности – Обособленности
+#define TABLE_THIRD_SCORE               "thirdresult"       //Колонка с результатами по Самоконтролю – Импульсивности
+#define TABLE_FOURTH_SCORE              "fourthresult"      //Колонка с результатами по Эмоциональной устойчивости – эмоциональной неустойчивости
+#define TABLE_FIFTH_SCORE               "fifthresult"       //Колонка с результатами по Экспрессивности – Практичности
+
+class DBclass : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DBclass(QObject *parent = nullptr);
+    void connectToDatabase();
+private:
+    bool openDataBase();        //Открытие БД
+    void closeDataBase();       //Закрытие БД
+private slots:
+    void on_newDataReceived(const QJsonArray &jsonArray);
+    void onHttpFinished(QNetworkReply *reply);
+public slots:
+    void insertIntoTable(const QVariantList &data, const int id);                                                                             //Вставить значения тестов в БД
+    void insertIntoTable(const QString &FirstResult, const QString &SecondResult, const QString &ThirdResult,                   //Подготовить массив данных для вставки в БД
+                         const QString &FourthResult, const QString &FifthResult, const int id);
+    void updateTable(const QVariantList &data, const int id);                                                                  //Изменить значения тестов в БД
+    void updateTable(const QString &FirstResult, const QString &SecondResult, const QString &ThirdResult,                     //Подготовить массив данных, которые будут использоваться в БД
+                     const QString &FourthResult, const QString &FifthResult, int id);
+    bool checkID();                                                                                                    //Проверить, что существует запись с ID пользователя
+    void get_results(int id);
+    int get_results1();                                                                                                //Получить результат первого фактора теста
+    int get_results2();                                                                                                //Получить результат второго фактора теста
+    int get_results3();                                                                                                //Получить результат третьего фактора теста
+    int get_results4();                                                                                                //Получить результат четвёртого фактора теста
+    int get_results5();                                                                                                //Получить результат пятого фактора теста
+signals:
+
+private:
+    QSqlDatabase db;
+    NetworkManager *networkManager;
+    int tempScore[5];
+    int flagUserID=0;
+};
+
+#endif // DBCLASS_H
